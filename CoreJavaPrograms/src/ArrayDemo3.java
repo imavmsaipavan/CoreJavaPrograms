5d28c206395edc6a8df8a9a78ddce37e2c3998a5
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-25 18:45:58 +0530

    new branch was created

--- CoreJavaPrograms/src/ArrayDemo3.java
+++ CoreJavaPrograms/src/ArrayDemo3.java
@@ -5,9 +5,16 @@ public class ArrayDemo3 {
 		  int[] arr= {1,2,4,54,332,4};
 	       for(int i=0;i<arr.length;i++)
 	       {
-	    	   System.out.println(arr[i]);
+	    	   System.out.print(arr[i]);
+	    	   System.out.print("");
 	       }
-		}
+	       int[] arr1= {1,432,544,54,332,4};
+	       for(int i=0;i<arr.length;i++)
+	       {
+	    	   System.out.print(arr[i]);
+	    	   System.out.print("");
+	       }
+	}
 	
 
 }

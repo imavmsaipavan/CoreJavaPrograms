7d20e68575bcbfdf96578461bfca6806a5d706ca
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-25 14:31:03 +0530

    VariableDemo2 Class was added

---
+++ CoreJavaPrograms/src/VariableDemo2.java
@@ -0,0 +1,13 @@
+
+public class VariableDemo2 {
+  int i=10;
+  static int j=20;
+  // int i=30;   it's a duplicate variable as i already exist 
+		  static void main(String[] args) {
+			  int k=30;
+			  int i=30;
+		//	  int k=40; // duplicate variable
+
+	}
+
+}

50c683b877f4e5f7472bfb966ba2b082a53f81fc
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-25 18:41:49 +0530

    new branch

---
+++ CoreJavaPrograms/src/ArrayDemo3.java
@@ -0,0 +1,13 @@
+
+public class ArrayDemo3 {
+
+	public static void main(String[] args) {
+		  int[] arr= {1,2,4,54,332,4};
+	       for(int i=0;i<arr.length;i++)
+	       {
+	    	   System.out.println(arr[i]);
+	       }
+		}
+	
+
+}

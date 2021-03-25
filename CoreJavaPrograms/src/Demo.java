7949a60a7ac91edd69a7a619ac4e4dbf84f300fa
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-25 14:21:05 +0530

    Demo class was added

---
+++ CoreJavaPrograms/src/Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+  /*
+    System.out.println("hello World");
+	because execution starts from main method
+     */
+	
+	public static void main(String[] args) {
+       System.out.println("Hello World");
+	}
+
+}

6f100159617d416d6d3365aa778b1edc604eb648
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-25 18:56:16 +0530

    conflit demo

--- CoreJavaPrograms/src/VariableDemo2.java
+++ CoreJavaPrograms/src/VariableDemo2.java
@@ -6,9 +6,11 @@ public class VariableDemo2 {
 		  static void main(String[] args) {
 			 
 			  
-			  int k=10;
-			  int i=10;
-			  int c=k+i;
+			  int a=55;
+			  int b=99;
+			  int c=a+b;
+			  
+			  
 		//	  int k=40; // duplicate variable
 
 	}

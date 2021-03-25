2f30d8cd19ba6f750022faf3b0126998ef951751
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-25 18:08:19 +0530

    variables values changed for second time

--- CoreJavaPrograms/src/VariableDemo2.java
+++ CoreJavaPrograms/src/VariableDemo2.java
@@ -4,8 +4,10 @@ public class VariableDemo2 {
   static int j=20;
   // int i=30;   it's a duplicate variable as i already exist 
 		  static void main(String[] args) {
-			  int k=30;
-			  int i=30;
+			  int k=35;
+			  int i=35;
+			  int c=k-i;
+			  
 		//	  int k=40; // duplicate variable
 
 	}

06a83c3731b19d9ff8f2eba35d3d0a06a37fa9b6
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-29 20:25:43 +0530

    commit for arrayDemo1

--- CoreJavaPrograms/src/ArrayDemo1.java
+++ CoreJavaPrograms/src/ArrayDemo1.java
@@ -2,11 +2,10 @@
 public class ArrayDemo1 {
 
 	public static void main(String[] args) {
+		System.out.println("this is for demo purpose");
      int[] arr= {1,2,3,4,5,6};
      for(int i=0;i<arr.length;i++) {
-    	 System.out.print(arr[i]);
-    	 System.out.print(" ");
-     }
+}
 	}
 
 }

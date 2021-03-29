765445f855a566f612c74e1d678cf33cb186a453
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-29 20:11:10 +0530

    arrayDemo3 is modified

--- CoreJavaPrograms/src/ArrayDemo3.java
+++ CoreJavaPrograms/src/ArrayDemo3.java
@@ -8,7 +8,7 @@ public class ArrayDemo3 {
 	    	   System.out.print(arr[i]);
 	    	   System.out.print("");
 	       }
-	       int[] arr1= {1,432,544,54,332,4};
+	       int[] arr1= {1,54,514,32,4};
 	       for(int i=0;i<arr.length;i++)
 	       {
 	    	   System.out.print(arr[i]);

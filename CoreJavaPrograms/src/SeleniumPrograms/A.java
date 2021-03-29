12c47db9d3c4b2089a7b4dcd921ed5c03386fdcf
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-29 17:41:56 +0530

    reverted back to fourth line

--- CoreJavaPrograms/src/SeleniumPrograms/A.java
+++ CoreJavaPrograms/src/SeleniumPrograms/A.java
@@ -7,8 +7,6 @@ public class A {
        String str1="second line"; 
        String str2="third line";
       String str3="fourth line";
-      String str4="fifth line"; 
-      String str5="seventh line";
 	}
 
 }

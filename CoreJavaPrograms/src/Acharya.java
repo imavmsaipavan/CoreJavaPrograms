585dab6f2e92fc035e26017d16cfc4ac23cb073c
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-29 20:27:16 +0530

    modified ACHARY AND VAKEEL SAB CLASSES

--- CoreJavaPrograms/src/Acharya.java
+++ CoreJavaPrograms/src/Acharya.java
@@ -4,6 +4,7 @@ public class Acharya {
 	public static void main(String[] args) {
      String Hero="MEGA STAR CHIRANJEEVI";
      String Heroine= "KAJAL";
+     String Director="KORATALA SIVA";
 	}
 
 }

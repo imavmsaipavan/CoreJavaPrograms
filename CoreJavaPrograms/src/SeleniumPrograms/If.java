e49054fc307cd7dbc266ff5f270d6417656c5620
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-31 19:30:00 +0530

    conflit will occur

--- CoreJavaPrograms/src/SeleniumPrograms/If.java
+++ CoreJavaPrograms/src/SeleniumPrograms/If.java
@@ -3,11 +3,11 @@ package SeleniumPrograms;
 public class If {
 
 	public static void main(String[] args) {
-		boolean a=false;
-		boolean b=true;
-		boolean c=true;
-		boolean f=false;
-		boolean d=true;
+		boolean a=true;
+		boolean b=false;
+		boolean c=false;
+		boolean f=true;
+		boolean d=false;
 
 		
 	}

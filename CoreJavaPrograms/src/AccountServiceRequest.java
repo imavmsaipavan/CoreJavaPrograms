e3b10500f3943615ddfbe5056d369314ef6c5f79
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-31 19:14:02 +0530

    account service request feature is added

---
+++ CoreJavaPrograms/src/AccountServiceRequest.java
@@ -0,0 +1,13 @@
+
+public class AccountServiceRequest {
+
+	public static void main(String[] args) {
+        System.out.println("this is account service request class");
+        System.out.println("this is account service request class");
+
+        System.out.println("this is account service request class");
+        System.out.println("this is account service request class");
+
+	}
+
+}

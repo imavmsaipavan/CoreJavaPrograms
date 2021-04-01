8ff1e2f7db2362df0538579cda45d4960a1dcda4
admin <admin@DESKTOP-NSN8G8G>
2021-04-01 17:22:47 +0530

    created function for debitcard pin generate

---
+++ CoreJavaPrograms/src/SeleniumPrograms/SivaBranch.java
@@ -0,0 +1,12 @@
+package SeleniumPrograms;
+
+public class SivaBranch {
+
+	public static void main(String[] args) {
+  String str="new Branch Demo";
+	}
+	public void pinGenerate(){
+		System.out.println("pin generate function");
+	}
+
+}
